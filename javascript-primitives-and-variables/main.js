6227203b4808693faf562fdc3e07795a04130b78
Joshua Sy Go <joshsygobusiness@gmail.com>
2021-08-23 10:48:49 -0700

    Completed javascript-primitives-and-variables exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,24 @@
+var fullName = 'Joshua Sy Go';
+
+var isCool = true;
+
+var totalPets = 0;
+
+var unknown = null;
+
+var never;
+
+console.log('fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+console.log('isCool:', isCool);
+console.log('typeof isCool:', typeof isCool);
+
+console.log('totalPets:', totalPets);
+console.log('typeof totalPets:', typeof totalPets);
+
+console.log('unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+
+console.log('never:', never);
+console.log('typeof never:', typeof never);
