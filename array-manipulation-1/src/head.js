fe0a377ab5a7f9d2a8161ac0611facfcd39d8382
Joshua Sy Go <joshsygobusiness@gmail.com>
2021-08-27 15:50:01 -0700

    in progress.

---
+++ array-manipulation-1/src/head.js
@@ -0,0 +1,9 @@
+/* exported head */
+// input: an array
+// output: the first element of the array
+// find first element of array
+// return first element of array
+
+function head(array) {
+  return array[0];
+}
---
+++ array-manipulation-1/src/tail.js
@@ -0,0 +1,3 @@
+/* exported tail */
+// input: an array
+// output: a NEW array containing all elements of old array except for the first one
