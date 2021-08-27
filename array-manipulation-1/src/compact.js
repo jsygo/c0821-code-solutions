cc0404d39ab763e462b861d91590c1c9ffa88f96
Joshua Sy Go <joshsygobusiness@gmail.com>
2021-08-27 16:43:50 -0700

    Completed array-manipulation-1

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -1,1 +1,17 @@
 /* exported compact */
+// input: an array
+// output: a NEW array containing all TRUTHY elements of the original array in the same order
+// create a new array for final output
+// check each element of array for truthiness
+// if truthy, add to final output array
+// return final output array
+
+function compact(array) {
+  var truthyArr = [];
+  for (var i = 0; i < array.length; i++) {
+    if (array[i]) {
+      truthyArr.push(array[i]);
+    }
+  }
+  return truthyArr;
+}
--- array-manipulation-1/src/reverse.js
+++ array-manipulation-1/src/reverse.js
@@ -1,1 +1,15 @@
 /* exported reverse */
+// input: an array
+// output: a NEW array containing all the elements of the original array, in reverse order
+// create new array for final output
+// go through each element in the array starting from the end
+// add each element you go over into final output array
+// return final output array
+
+function reverse(array) {
+  var reverseArr = [];
+  for (var i = (array.length - 1); i >= 0; i--) {
+    reverseArr.push(array[i]);
+  }
+  return reverseArr;
+}
