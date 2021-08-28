d83f1760f2b6054471ac84456718b7e1ee93c06b
Joshua Sy Go <joshsygobusiness@gmail.com>
2021-08-27 18:09:00 -0700

    in progress

--- string-manipulation-2/src/truncate.js
+++ string-manipulation-2/src/truncate.js
@@ -1,1 +1,18 @@
 /* exported truncate */
+// input: a length which will be a number, a string
+// output: a string that is only as long as the value of the 'length' variable, with '...' appended
+// output cont.: if string is shorter than length, return string plus '...'
+// create storage for shortened string
+// go through as many characters of the string as is the value of 'length' variable
+// add each character you go over to the storage for the shortened string
+// append '...' to your shortened string
+// return shortened string
+
+function truncate(length, string) {
+  var truncatedStr = '';
+  for (var i = 0; (i < length) && (i < string.length); i++) {
+    truncatedStr += string[i];
+  }
+  truncatedStr += '...';
+  return truncatedStr;
+}
