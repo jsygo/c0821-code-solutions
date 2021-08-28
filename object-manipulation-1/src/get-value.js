0c9a4897c4e864661b22d2642d2d4347b0a5aa42
Joshua Sy Go <joshsygobusiness@gmail.com>
2021-08-27 17:27:09 -0700

    Completed change request

--- object-manipulation-1/src/get-value.js
+++ object-manipulation-1/src/get-value.js
@@ -1,13 +1,10 @@
 /* exported getValue */
 // input: an object, a key which will be a string
 // output: the value of the property of the object who's name matches key
-// check through each property of the object
-// if property matches key, return the value of that property
+// NOT NEEDED: check through each property of the object
+// NOT NEEDED: if property matches key, return the value of that property
+// revision: return the value of the property of the object who's name matches key
 
 function getValue(object, key) {
-  for (var property in object) {
-    if (property === key) {
-      return object[property];
-    }
-  }
+  return object[key];
 }
