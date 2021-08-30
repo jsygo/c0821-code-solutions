6fd2070bd08cdd0ad5f4e369b32b19574067a7d1
Joshua Sy Go <joshsygobusiness@gmail.com>
2021-08-30 15:23:58 -0700

    in progress

--- array-manipulation-2/src/includes.js
+++ array-manipulation-2/src/includes.js
@@ -1,1 +1,16 @@
 /* exported includes */
+// input: an array, a value
+// output: a Boolean indiciating whether or not the value is present at any idex of the array
+// check through each element in array
+//    each time compare element to value
+//      if the same, return true
+// otherwise, return false
+
+function includes(array, value) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === value) {
+      return true;
+    }
+  }
+  return false;
+}
