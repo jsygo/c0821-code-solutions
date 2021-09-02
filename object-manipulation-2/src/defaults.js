3cf45c2e21048e3a5629e1233df00161f64e66d1
Joshua Sy Go <joshsygobusiness@gmail.com>
2021-09-01 18:12:35 -0700

    Completed object-manipulation-2.

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -3,22 +3,44 @@
 // output: no return, the function assigns properties of source object to the target object
 // for all properties that are not defined on target
 
-// check through each property of the source object
-//    each time, check if each property of the target object is equal to that property
-//        if it is, move to the next property of source
-//        if it is not, add that property to target
+// for each property of source,
+//    check each property in target
+//      if it matches, do not update target, continue to next property of source
+//      if it does not match, move to the next property of source
+//      if you have checked each property of the object, update the target
+
+// function defaults(target, source) {
+//   var i = 0;
+//   for (var sourceKey in source) {
+//     for (var targetKey in target) {
+//       if (sourceKey === targetKey) {
+//         break;
+//       } else if (sourceKey !== targetKey && i === target.length - 1) {
+//         target[sourceKey] = source[sourceKey];
+//       } else if (sourceKey !== targetKey) {
+//         i++;
+//       }
+//     }
+//   }
+// }
+
+// create copy of target object
+// add all properties of source to target
+// compare targetCopy to target and replace any properties that match with the value from targetCopy
 
 function defaults(target, source) {
-  var i = 0;
-  debugger;
+  var targetCopy = {};
+  for (var targetKey in target) {
+    targetCopy[targetKey] = target[targetKey];
+  }
   for (var sourceKey in source) {
-    for (var targetKey in target) {
-      if (targetKey === sourceKey) {
-        break;
-      } else if (i === target.length - 1) {
-        target[sourceKey] = source[sourceKey];
+    target[sourceKey] = source[sourceKey];
+  }
+  for (var targetCopyKey in targetCopy) {
+    for (targetKey in target) {
+      if (targetKey === targetCopyKey) {
+        target[targetKey] = targetCopy[targetCopyKey];
       }
-      i++;
     }
   }
 }
