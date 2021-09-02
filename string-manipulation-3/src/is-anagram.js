838975aa778af6734534ed6c602e36b4dffccd60
Joshua Sy Go <joshsygobusiness@gmail.com>
2021-09-02 16:58:56 -0700

    in progress

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,1 +1,38 @@
 /* exported isAnagram */
+// input: firstString, secondString
+// output: a Boolean indicating whether or not second string is an anagram for the first string
+// create an array for all the character in the first string
+// add all characters of the first string into that array
+// create an array for all the characters in the first string
+// add all characters from the second string into the second array
+// go through each element in first array
+//    check if its value matches the value of any element in the second array
+//        if it matches, delete those elements from the second array and move on to the next character in the first array
+// check if both arrays are empty
+//    if they are, return true
+//    otherwise, return false
+
+function isAnagram(firstString, secondString) {
+  var firstNoSpace = firstString.replaceAll(' ', '');
+  var secondNoSpace = secondString.replaceAll(' ', '');
+  var firstCharacters = [];
+  for (var i = 0; i < firstNoSpace.length; i++) {
+    firstCharacters.push(firstNoSpace[i]);
+  }
+  var secondCharacters = [];
+  for (var j = 0; j < secondNoSpace.length; j++) {
+    secondCharacters.push(secondNoSpace[j]);
+  }
+  for (var k = 0; k < firstCharacters.length; k++) {
+    for (var m = 0; m < secondCharacters.length; m++) {
+      if (firstCharacters[k] === secondCharacters[m]) {
+        secondCharacters.splice(m, 1);
+        break;
+      }
+    }
+  }
+  if (secondCharacters.length === 0) {
+    return true;
+  }
+  return false;
+}
--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,39 @@
 /* exported reverseWords */
+// input: a string
+// output: a version of the string with all words reversed but the words in original order
+// I can loop through the string and go until i find a space. I need a variable that will tell me
+// the index of the previous space. this will start at -1 and get updated when i find a space.
+// when i do get to a space, iterate backwards through the string until the index after the last space
+// each character will get added to the new string, and then the space i am on will get added
+
+// create new string
+// create a variable for the previous space, which will start at -1 (because the first index i need to put
+// in the new string is 0)
+// loop through the string
+//    if the character im at is a space
+//        go backwards over each character starting previous to it until the index after the index of the last space
+//        add each character into new string
+//        add space into string
+//        set previous space variable to current index
+//    if at end of string, go backwards til previous space and add characters to string
+// return new string
+
+function reverseWords(string) {
+  var reversedWords = '';
+  var previousSpace = -1;
+  for (var i = 0; i < string.length; i++) {
+    if (string[i] === ' ') {
+      for (var j = i - 1; j > previousSpace; j--) {
+        reversedWords += string[j];
+      }
+      reversedWords += string[i];
+      previousSpace = i;
+    }
+    if (i === string.length - 1) {
+      for (var k = i; k > previousSpace; k--) {
+        reversedWords += string[k];
+      }
+    }
+  }
+  return reversedWords;
+}
