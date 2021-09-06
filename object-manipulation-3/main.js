d3c091c40396f2271e709a5d40259e753228d938
Joshua Sy Go <joshsygobusiness@gmail.com>
2021-09-06 16:36:07 -0700

    in progress

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,58 +1,134 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
 
 var players = [];
+for (var playersIndex = 0; playersIndex < 4; playersIndex++) {
+  players.push({ name: `player${playersIndex + 1}`, hand: [] });
+}
 
-var deck = [
-  { rank: 'ace', suit: 'clubs', value: 11 },
-  { rank: 2, suit: 'clubs', value: 2 },
-  { rank: 3, suit: 'clubs', value: 3 },
-  { rank: 4, suit: 'clubs', value: 4 },
-  { rank: 5, suit: 'clubs', value: 5 },
-  { rank: 6, suit: 'clubs', value: 6 },
-  { rank: 7, suit: 'clubs', value: 7 },
-  { rank: 8, suit: 'clubs', value: 8 },
-  { rank: 9, suit: 'clubs', value: 9 },
-  { rank: 10, suit: 'clubs', value: 10 },
-  { rank: 'jack', suit: 'clubs', value: 10 },
-  { rank: 'queen', suit: 'clubs', value: 10 },
-  { rank: 'king', suit: 'clubs', value: 10 },
-  { rank: 'ace', suit: 'diamonds', value: 11 },
-  { rank: 2, suit: 'diamonds', value: 2 },
-  { rank: 3, suit: 'diamonds', value: 3 },
-  { rank: 4, suit: 'diamonds', value: 4 },
-  { rank: 5, suit: 'diamonds', value: 5 },
-  { rank: 6, suit: 'diamonds', value: 6 },
-  { rank: 7, suit: 'diamonds', value: 7 },
-  { rank: 8, suit: 'diamonds', value: 8 },
-  { rank: 9, suit: 'diamonds', value: 9 },
-  { rank: 10, suit: 'diamonds', value: 10 },
-  { rank: 'jack', suit: 'diamonds', value: 10 },
-  { rank: 'queen', suit: 'diamonds', value: 10 },
-  { rank: 'king', suit: 'diamonds', value: 10 },
-  { rank: 'ace', suit: 'spades', value: 11 },
-  { rank: 2, suit: 'spades', value: 2 },
-  { rank: 3, suit: 'spades', value: 3 },
-  { rank: 4, suit: 'spades', value: 4 },
-  { rank: 5, suit: 'spades', value: 5 },
-  { rank: 6, suit: 'spades', value: 6 },
-  { rank: 7, suit: 'spades', value: 7 },
-  { rank: 8, suit: 'spades', value: 8 },
-  { rank: 9, suit: 'spades', value: 9 },
-  { rank: 10, suit: 'spades', value: 10 },
-  { rank: 'jack', suit: 'spades', value: 10 },
-  { rank: 'queen', suit: 'spades', value: 10 },
-  { rank: 'king', suit: 'spades', value: 10 },
-  { rank: 'ace', suit: 'hearts', value: 11 },
-  { rank: 2, suit: 'hearts', value: 2 },
-  { rank: 3, suit: 'hearts', value: 3 },
-  { rank: 4, suit: 'hearts', value: 4 },
-  { rank: 5, suit: 'hearts', value: 5 },
-  { rank: 6, suit: 'hearts', value: 6 },
-  { rank: 7, suit: 'hearts', value: 7 },
-  { rank: 8, suit: 'hearts', value: 8 },
-  { rank: 9, suit: 'hearts', value: 9 },
-  { rank: 10, suit: 'hearts', value: 10 },
-  { rank: 'jack', suit: 'hearts', value: 10 },
-  { rank: 'queen', suit: 'hearts', value: 10 },
-  { rank: 'king', suit: 'hearts', value: 10 }
-];
+var deck = [];
+
+for (var deckIndex = 0; deckIndex < 52; deckIndex++) {
+  deck.push({ rank: '', suit: '' });
+  if (deckIndex < 13) {
+    deck[deckIndex].suit = 'clubs';
+    if (deckIndex < 9) {
+      deck[deckIndex].rank = deckIndex + 2;
+    } else if (deckIndex === 9) {
+      deck[deckIndex].rank = 'jack';
+    } else if (deckIndex === 10) {
+      deck[deckIndex].rank = 'queen';
+    } else if (deckIndex === 11) {
+      deck[deckIndex].rank = 'king';
+    } else if (deckIndex === 12) {
+      deck[deckIndex].rank = 'ace';
+    }
+  } else if (deckIndex < 26) {
+    deck[deckIndex].suit = 'diamonds';
+    if (deckIndex < 22) {
+      deck[deckIndex].rank = deckIndex - 11;
+    } else if (deckIndex === 22) {
+      deck[deckIndex].rank = 'jack';
+    } else if (deckIndex === 23) {
+      deck[deckIndex].rank = 'queen';
+    } else if (deckIndex === 24) {
+      deck[deckIndex].rank = 'king';
+    } else if (deckIndex === 25) {
+      deck[deckIndex].rank = 'ace';
+    }
+  } else if (deckIndex < 39) {
+    deck[deckIndex].suit = 'spades';
+    if (deckIndex < 35) {
+      deck[deckIndex].rank = deckIndex - 24;
+    } else if (deckIndex === 35) {
+      deck[deckIndex].rank = 'jack';
+    } else if (deckIndex === 36) {
+      deck[deckIndex].rank = 'queen';
+    } else if (deckIndex === 37) {
+      deck[deckIndex].rank = 'king';
+    } else if (deckIndex === 38) {
+      deck[deckIndex].rank = 'ace';
+    }
+  } else {
+    deck[deckIndex].suit = 'hearts';
+    if (deckIndex < 48) {
+      deck[deckIndex].rank = deckIndex - 37;
+    } else if (deckIndex === 48) {
+      deck[deckIndex].rank = 'jack';
+    } else if (deckIndex === 49) {
+      deck[deckIndex].rank = 'queen';
+    } else if (deckIndex === 50) {
+      deck[deckIndex].rank = 'king';
+    } else if (deckIndex === 51) {
+      deck[deckIndex].rank = 'ace';
+    }
+  }
+}
+// First i need helper functions for:
+// dealing the cards
+//    shuffling should be the first thing that happens in the game function
+// calculating score and winner
+
+// for shuffling cards
+
+var shuffledDeck = _.shuffle(deck);
+
+function dealCards(players, deck, numOfCards) {
+  for (var i = 0; i < players.length; i++) {
+    for (var j = 0; j < numOfCards; j++) {
+      var currentCard = deck.shift();
+      players[i].hand.push(currentCard);
+    }
+  }
+}
+
+// add score property to each player
+// loop through each players hand
+//    if card is a number, add that value to score
+//    if it is not, and it is not an ace, add 11
+//    otherwise, add 11
+
+function getScore(player) {
+  player.score = 0;
+  debugger;
+  for (var i = 0; i < player.hand; i++) {
+    if (typeof player.hand[i].rank === 'number') {
+      player.score += player.hand[i].rank;
+    } else if (player.hand[i].rank !== 'ace') {
+      player.score += 10;
+    } else {
+      player.score += 11;
+    }
+  }
+}
+
+// create an array for the winner(s)
+// create a variable for the highest score which starts at zero
+// find the sum of all the cards values in each players hand
+// assign that value to a new property of each player: score
+// check each players score
+// if it is higher than highestScore, change highestScore to the value of that players score
+//    replace the player in the winners array with that player
+// if it is equal to highestScore, add that player to the winners array
+
+function findWinner(players) {
+  var winners = [];
+  var highestScore = 0;
+  // debugger;
+  for (var i = 0; i < players.length; i++) {
+    getScore(players[i]);
+    if (players[i].score > highestScore) {
+      highestScore = players[i].score;
+      winners = [];
+      winners.push(players[i]);
+    } else if (players[i].score === highestScore) {
+      winners.push(players[i]);
+    }
+  }
+  return winners;
+}
+
+dealCards(players);
+findWinner(players);
+
+// shuffle cards
+//
