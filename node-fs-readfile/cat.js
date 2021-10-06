c9233cdbce5f345b03ed697ccca60b2c8b3f60ed
jsygo <joshsygobusiness@gmail.com>
2021-10-06 23:47:07 +0000

    Completed node-fs-readfile level 3

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -9,6 +9,10 @@ for (let i = 2; i < process.argv.length; i++) {
   fs.readFile(`./${process.argv[i]}`, 'utf8', (err, text) => {
     if (err) throw err;
     textArr[i - 2] = text;
-    console.log(textArr);
+    if (!textArr.includes(null)) {
+      for (let j = 0; j < textArr.length; j++) {
+        console.log(textArr[j]);
+      }
+    }
   });
 }
