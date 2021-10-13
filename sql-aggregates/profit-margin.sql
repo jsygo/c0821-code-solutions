818341be4ffcc838973f04b6003531196e451fc2
jsygo <joshsygobusiness@gmail.com>
2021-10-13 20:02:33 +0000

    profit-margin in progress

--- sql-aggregates/profit-margin.sql
+++ sql-aggregates/profit-margin.sql
@@ -1,30 +1,48 @@
-with "revenue" as (
+-- with "revenue" as (
+--   select sum("films"."rentalRate") as "totalRevenue",
+--           "films"."title" as "title"
+--     from "films"
+--     join "inventory" using ("filmId")
+--     join "rentals" using ("inventoryId")
+--   group by "films"."filmId"
+-- ),
+-- "cost" as (
+--   select sum("films"."replacementCost") as "totalCost",
+--          "films"."title" as "title"
+--     from "films"
+--     join "inventory" using ("filmId")
+--   group by "films"."filmId"
+-- ),
+-- "profit" as (
+--   select ("revenue"."totalRevenue" - "cost"."totalCost") as "totalProfit",
+--          "films"."title"
+--     from "revenue"
+--     join "cost" using ("title")
+--     join "films" using ("title")
+-- )
+
+with "cost" as (
+  select sum("films"."replacementCost") as "totalCost",
+         "films"."title"
+    from "films"
+    join "inventory" using ("filmId")
+  group by "filmId"
+),
+"revenue" as (
   select sum("films"."rentalRate") as "totalRevenue",
-          "films"."title" as "title"
+         "films"."title"
     from "films"
     join "inventory" using ("filmId")
     join "rentals" using ("inventoryId")
-  group by "films"."filmId"
-),
-"profit" as (
-  select ("revenue"."totalRevenue" - "films"."replacementCost") as "totalProfit",
-         "films"."title"
-    from "revenue"
-    join "films" using ("title")
+  group by "filmId"
 )
-
--- with "profit" as (
---   select (sum("films"."rentalRate") - "films"."replacementCost") as "totalProfit",
---          "films"."title"
---     from "films"
---   group by "films"."title"
--- )
 
 select "films"."title",
        "films"."description",
        "films"."rating",
-       "profit"."totalProfit"
+       ("revenue"."totalRevenue" - "cost"."totalCost") as "totalProfit"
   from "films"
-  join "profit" using ("title")
+  join "cost" using ("title")
+  join "revenue" using ("title")
 order by "totalProfit" desc
  limit 5;
