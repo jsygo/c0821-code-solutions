5b9b431fd06168203d4fe40b9918fc51adda0439
jsygo <joshsygobusiness@gmail.com>
2021-10-25 20:20:25 +0000

    added catch

--- fetch/main.js
+++ fetch/main.js
@@ -1,3 +1,4 @@
 fetch('https://jsonplaceholder.typicode.com/users')
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => console.log(data))
+  .catch(err => console.error('Fetch failed!', err));
